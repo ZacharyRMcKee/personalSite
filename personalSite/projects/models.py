955a7a4fc64e06afa7bbbff376f63c1e7c6c763f
Zachary R. McKee <zmckee@hawk.iit.edu>
2017-12-19 01:04:25 -0600

    Another terrible commit

--- personalSite/projects/models.py
+++ personalSite/projects/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Project(models.Model):
 	name = models.CharField(max_length=50)
 	source = models.URLField()
+	summary = models.TextField()
 	contents = models.TextField()
 	def __str__(self):
 		return self.name
